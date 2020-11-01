ea6e218a5fd7ca3020bda56157e6763efbe1817b
andru <andrugor1994@gmail.com>
2020-11-01 22:52:31 +0300

    fixed

--- src/main/java/com/example/demo/models/ClientMaster.java
+++ src/main/java/com/example/demo/models/ClientMaster.java
@@ -1,24 +1,24 @@
 package com.example.demo.models;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.time.LocalDate;
 
 @Data
 @Entity
-public class Day {
+public class ClientMaster {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Поле не может быть пустым")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate localDate;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional=false, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 
     @ManyToOne(fetch = FetchType.EAGER, optional=false, cascade = CascadeType.ALL)
     @JoinColumn(name = "client_id")
     private Client client;
+
 }
